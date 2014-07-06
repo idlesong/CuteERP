7bb69bda9fc08180bf5305fde06f148228e65c33
idlesong <idlesong@gmail.com>
2014-07-06 17:40:22 +0800

    bugfix for migration

--- db/migrate/20140627063347_add_customer_id_to_opportunity.rb
+++ db/migrate/20140627063347_add_customer_id_to_opportunity.rb
@@ -1,5 +1,5 @@
 class AddCustomerIdToOpportunity < ActiveRecord::Migration
   def change
-    add_column :opportunities, :customer_id, :inter
+    add_column :opportunities, :customer_id, :integer
   end
 end
--- db/migrate/20140627064028_add_opportunity_id_to_oppostatus.rb
+++ db/migrate/20140627064028_add_opportunity_id_to_oppostatus.rb
@@ -1,5 +1,5 @@
 class AddOpportunityIdToOppostatus < ActiveRecord::Migration
   def change
-    add_column :oppostatuses, :opportunity_id, :inter
+    add_column :oppostatuses, :opportunity_id, :integer
   end
 end
