a5c842b80d2f08d51a8b12d384fdd1fdaa629970
idlesong <idlesong@gmail.com>
2015-01-09 18:00:02 +0800

    add order content and bug fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,7 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @posts = Post.all
     @post = Post.find(params[:id])
 
     respond_to do |format|
