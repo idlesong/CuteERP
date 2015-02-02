9b5f31fe274810329159e513f3699dd253f25831
idlesong <idlesong@gmail.com>
2015-02-02 15:57:14 +0800

    debug paperclip of attachments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   attr_accessible :content, :subject, :title, :internal_url
   validates :internal_url, :uniqueness => true
 
-  #has_many :attchments, :dependent => :destroy
+  has_many :attchments, :dependent => :destroy
 end
