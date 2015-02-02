342479ab6f536c6851d3de4d9f77b4e6ca8ceb62
idlesong <idlesong@gmail.com>
2015-02-02 15:25:03 +0800

    disable post attachment to debug

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,7 +1,9 @@
 class Attachment < ActiveRecord::Base
   attr_accessible :description, :name, :document
 
+  #belongs_to :post
+
   validates_attachment :document, :content_type => {:content_type =>
-    %w(image/jpeg image/jpg image/png application/pdf application/7-zip application/msword
+    %w(image/jpeg image/jpg image/png application/pdf application/msword
     application/vnd.openxmlformats-officedocument.wordprocessingml.document)}
 end
