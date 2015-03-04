c94da000e69eaa260ad6cfe96b6c60283a402ff5
idlesong <idlesong@gmail.com>
2015-03-04 21:41:09 +0800

    open precompile assets

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,6 @@ module RorWebERP
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    #config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
   end
 end
