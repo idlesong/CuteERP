24d69bd5d87c94a1365689cf3c6a6f523aec2b82
idlesong <idlesong@gmail.com>
2016-07-08 11:14:08 +0800

    setup heroku assets precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ RorWebERP::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -47,7 +47,7 @@ RorWebERP::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
-    config.assets.precompile += ['pdf.css', 'pdf.js']   
+    config.assets.precompile += ['pdf.css', 'pdf.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
