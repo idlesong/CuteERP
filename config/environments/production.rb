6606a001015abad35211af8b5948969076f736e9
idlesong <idlesong@gmail.com>
2022-05-02 19:23:47 +0800

    enable eager_load in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ RorWebERP::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_files = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -66,6 +66,10 @@ RorWebERP::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  config.log_level = :info  
+
   #debug
   config.consider_all_requests_local = true
+
+  config.eager_load = true
 end
